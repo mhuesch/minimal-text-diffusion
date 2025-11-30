7f4e61a1e78478e58411c8e1c25553468fffa6e1
Michael Hueschen <m@mhueschen.space>
2025-11-30 12:53:03 -0800

    no wandb

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -14,7 +14,6 @@ import tempfile
 import warnings
 from collections import defaultdict
 from contextlib import contextmanager
-import wandb
 
 DEBUG = 10
 INFO = 20
@@ -367,7 +366,6 @@ class Logger(object):
             if self.comm.rank != 0:
                 d["dummy"] = 1  # so we don't get a warning about empty dict
         # LISA
-        wandb.log({**d})
         out = d.copy()  # Return the dict for unit testing purposes
         for fmt in self.output_formats:
             if isinstance(fmt, KVWriter):
