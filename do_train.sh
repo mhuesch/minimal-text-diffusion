bef30ab5dd8754ab000e3e818070d35a44b2e3ab
Michael Hueschen <m@mhueschen.space>
2025-11-30 12:46:49 -0800

    fix path

--- do_train.sh
+++ do_train.sh
@@ -3,7 +3,7 @@ source env/bin/activate
 export PYTHONPATH="$(pwd)/src:$PYTHONPATH"
 
 python src/train_infer/train.py \
-  --train_txt_path data/simple/train.txt \
+  --train_txt_path data/simple-train.txt \
   --sequence_len 50 \
   --batch_size 32 \
   --modality text \
