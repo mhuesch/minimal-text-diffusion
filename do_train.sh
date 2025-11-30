dd356319a9a48472617446964d89667313616b24
Michael Hueschen <m@mhueschen.space>
2025-11-30 13:04:04 -0800

    more stupid config

--- do_train.sh
+++ do_train.sh
@@ -4,6 +4,8 @@ export PYTHONPATH="$(pwd)/src:$PYTHONPATH"
 
 python src/train_infer/train.py \
   --train_txt_path data/simple-train.txt \
+  --val_txt_path data/simple-test.txt \
+  --dataset simple \
   --sequence_len 50 \
   --batch_size 32 \
   --modality text \
