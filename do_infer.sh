ad98b383bb6c61137e69ec06525a365a46211be1
Michael Hueschen <m@mhueschen.space>
2025-12-03 08:56:50 -0800

    make inference sequence_len match training (50)

--- do_infer.sh
+++ do_infer.sh
@@ -7,5 +7,5 @@ python src/train_infer/text_sample.py \
   --model_name_or_path ./checkpoints/ema_0.9999_030000.pt \
   --num_samples 8 \
   --diffusion_steps 4000 \
-  --sequence_len 32 \
+  --sequence_len 50 \
   --use_bert_tokenizer True
